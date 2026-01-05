da315b86c4bd5b922aded1ca885d659d6a54e014
Taeyang1188 <golandogeonseo@gmail.com>
2026-01-05 10:18:51 +0900

    Update ContactFAB.tsx

--- components/ContactFAB.tsx
+++ components/ContactFAB.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 const ContactFAB: React.FC = () => {
   return (
     <div className="fixed bottom-8 right-8 z-[100] flex flex-col space-y-4">
+      {/* 전화 상담 버튼 */}
       <a 
         href="tel:02-776-4778" 
         className="bg-[#A53837] w-14 h-14 rounded-full flex items-center justify-center text-white shadow-2xl hover:scale-110 transition-transform group"
@@ -16,6 +17,7 @@ const ContactFAB: React.FC = () => {
         </span>
       </a>
       
+      {/* 라이브 챗 버튼 (네이버 톡톡 연결) */}
       <a 
         href="https://talk.naver.com/ct/wargoq0?frm=home"
         target="_blank"
@@ -26,7 +28,7 @@ const ContactFAB: React.FC = () => {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
         </svg>
         <span className="absolute right-16 bg-white text-gray-900 px-3 py-1 rounded-md text-sm font-bold opacity-0 group-hover:opacity-100 transition-opacity shadow-sm whitespace-nowrap">
-          Live Chat
+          Live Chat (Naver Talk)
         </span>
       </a>
     </div>
