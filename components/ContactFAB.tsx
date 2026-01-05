53881aa50acd63af784bfcef465930c641e0a063
Taeyang1188 <golandogeonseo@gmail.com>
2026-01-05 10:15:13 +0900

    Update ContactFAB.tsx

--- components/ContactFAB.tsx
+++ components/ContactFAB.tsx
@@ -16,14 +16,19 @@ const ContactFAB: React.FC = () => {
         </span>
       </a>
       
-      <button className="bg-white w-14 h-14 rounded-full flex items-center justify-center text-[#A53837] shadow-2xl hover:scale-110 transition-transform group border border-gray-100">
+      <a 
+        href="https://talk.naver.com/ct/wargoq0?frm=home"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-white w-14 h-14 rounded-full flex items-center justify-center text-[#A53837] shadow-2xl hover:scale-110 transition-transform group border border-gray-100"
+      >
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
         </svg>
         <span className="absolute right-16 bg-white text-gray-900 px-3 py-1 rounded-md text-sm font-bold opacity-0 group-hover:opacity-100 transition-opacity shadow-sm whitespace-nowrap">
           Live Chat
         </span>
-      </button>
+      </a>
     </div>
   );
 };
